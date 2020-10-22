529dc6c68330b07e13d0ef8df1b5cdfd1bc01ee5
furkan-cloud <firstinfirstout2020@gmail.com>
2020-10-22 23:02:11 +0300

    navigation sending props project complete

--- src/Router.js
+++ src/Router.js
@@ -4,15 +4,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { Friends, Main, Post } from './pages'
+import { Friends, Timeline, Post } from './pages'
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function MainComponent() {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="MainPage" component={Main} />
+        <Stack.Navigator initialRouteName="MainPage">
+            <Stack.Screen name="MainPage" component={Timeline} />
             <Stack.Screen name="PostPage" component={Post} />
         </Stack.Navigator>
     )
@@ -21,7 +21,7 @@ function MainComponent() {
 function Router() {
     return (
         <NavigationContainer>
-            <Tab.Navigator initialRouteName="PostPage" tabBarOptions={{ activeTintColor: 'purple' }}>
+            <Tab.Navigator initialRouteName="MainComponentPage" tabBarOptions={{ activeTintColor: 'purple' }}>
                 <Tab.Screen name="FriendsPage" component={Friends} options={{ title: 'Arkadaşlar' }} />
                 <Tab.Screen name="MainComponentPage" component={MainComponent} options={{ title: 'Ana Sayfa' }} />
             </Tab.Navigator>
