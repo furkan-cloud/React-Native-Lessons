1c5e7ca4eedf9dff2306b8419541db883634f7d6
furkan-cloud <firstinfirstout2020@gmail.com>
2020-10-22 21:59:04 +0300

    tab and stack nav add

--- src/Router.js
+++ src/Router.js
@@ -1,26 +1,32 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 
-import First from './pages/First';
-import Second from './pages/Second';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Friends, Main, Post } from './pages'
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainComponent() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="MainPage" component={Main} />
+            <Stack.Screen name="PostPage" component={Post} />
+        </Stack.Navigator>
+    )
+}
 
 function Router() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          gestureEnabled: true,
-        }}>
-        <Stack.Screen name="FirstPage" component={First} />
-        <Stack.Screen name="SecondPage" component={Second} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="PostPage" tabBarOptions={{ activeTintColor: 'purple' }}>
+                <Tab.Screen name="FriendsPage" component={Friends} options={{ title: 'Arkadaşlar' }} />
+                <Tab.Screen name="MainComponentPage" component={MainComponent} options={{ title: 'Ana Sayfa' }} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default Router;
